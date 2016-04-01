a6498e52e93e26878ead06f2a4589610ee2dc38d
Wenyu Tang <danny.tang@a8.Millennium.Berkeley.EDU>
2016-04-01 16:51:25 -0700

    update testbench for progress on cover property dsl

--- results/cover_property_testbench/src/main/scala/CoverPropertyDriver.scala
+++ results/cover_property_testbench/src/main/scala/CoverPropertyDriver.scala
@@ -1,11 +1,14 @@
 package ChiselDriver
 
 import Chisel._
+import CoveragePropertyDSL._
 
 class ChiselDriver extends Module {
   val io = new Bundle { 
     val out = UInt(OUTPUT, 8)
   }
+  val testWire = UInt(1)
+  val a = new StartSequence(testWire === UInt(1))
   io.out := UInt(42)
 }
 
@@ -17,6 +20,7 @@ class ChiselDriverTests(c: ChiselDriver) extends Tester(c) {
 object ChiselDriver {
   def main(args: Array[String]): Unit = {
     val tutArgs = args.slice(1, args.length)
+    CoverProperty.testFunc()
     chiselMainTest(tutArgs, () => Module(new ChiselDriver())) {
       c => new ChiselDriverTests(c) }
   }
